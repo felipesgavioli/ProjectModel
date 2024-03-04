56fde9767c7d81db02e1f0fd4ea53e27f6de3636
felipe.sgavioli <felipe.sgavioli@farfetch.com>
2024-03-04 08:45:09 -0300

    add validation

--- ProjectModel.Domain/User.cs
+++ ProjectModel.Domain/User.cs
@@ -6,5 +6,12 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public User(string name, string email, string password) 
+        { 
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Email = email ?? throw new ArgumentNullException(nameof(name));
+            Password = password ?? throw new ArgumentNullException(nameof(name));
+        }
     }
 }
